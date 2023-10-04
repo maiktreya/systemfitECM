14cb0f8faf33c59696701eabe79e698dd752a0fa
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-04 21:23:58 +0200

    last

--- R/examples.R
+++ R/examples.R
@@ -1,10 +1,10 @@
 ###############################################
 # Define dataset of usage (data.table required) and selected variables for coint. analysis. The dependant var should be listed first.
 
-library(data.table) # for simple and performant data manipulation 
-library(plm) # needed for systemfit to handle panel structure 
-library(systemfit # for FGLS system linear models 
-library(magrittr) # For piping with %<% without dplyr dependencies 
+library(data.table) # for simple and performant data manipulation
+library(plm) # needed for systemfit to handle panel structure
+library(systemfit) # for FGLS system linear models
+library(magrittr) # For piping with %<% without dplyr dependencies
 library(aod) # for performing F Bounds test
 
 # install and import this library
