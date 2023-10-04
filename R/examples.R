7d3861197ce4dbb781245bba734802d4bb160932
iliciuv <72982503+iliciuv@users.noreply.github.com>
2023-10-04 21:00:33 +0200

    Update examples.R

--- R/examples.R
+++ R/examples.R
@@ -1,5 +1,6 @@
 ###############################################
 # Define dataset of usage (data.table required) and selected variables for coint. analysis. The dependant var should be listed first.
+
 library(data.table) # for simple and performant data manipulation 
 library(plm) # needed for systemfit to handle panel structure 
 library(systemfit # for FGLS system linear models 
