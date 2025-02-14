3acadbcdb0d717e1285d438ee6e812262d0f9a4a
maiktreya <miguel.garcia.duch@gmail.com>
2025-02-14 12:00:17 +0100

    Refactor documentation for get_ect_systemfit and systemfit functions; remove recm_systemfit documentation

--- tests/build_process.R
+++ tests/build_process.R
@@ -2,10 +2,12 @@
 
 rm(list = ls())
 gc(full = TRUE)
+unloadNamespace("systemfitECM")
 
-# devtools::document()
+devtools::document()
 devtools::load_all(reset = TRUE)
 devtools::install(dependencies = TRUE) # To ensure a clean install
 devtools::build()
 
-# install.packages("dist/systemfitECM_0.1.0.tar.gz",repos = NULL, type="source")
+# install.packages("dist/systemfitECM_0.1.0.tar.gz",repos = NULL, type="source") #nolint
+# devtools::install_github("iliciuv/systemfitECM", ref = "new-dev") #nolint
